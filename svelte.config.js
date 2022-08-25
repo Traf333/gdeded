0d3ab3023d0606d1445c159121f509906aac0801
Igor Trofimov <igor.trofimov@symmetre.com>
2022-08-25 09:23:26 +0200

    one more try

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
-
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,19 +8,8 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+		adapter: adapter(),
 
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		},
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
