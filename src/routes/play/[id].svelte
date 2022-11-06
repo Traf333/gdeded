f1425168ffd6424f9a178c7a002480c837f48a68
Igor Trofimov <igor.trofimov@symmetre.com>
2022-11-06 16:09:17 +0200

    cleanup

--- src/routes/play/[id].svelte
+++ src/routes/play/[id].svelte
@@ -1,7 +1,3 @@
-<script context="module" lang="ts">
-  export const prerender = true;
-</script>
-
 <script lang="ts">
   import type { IScenario, ISpeech } from '$lib/types';
   import { page } from '$app/stores';
