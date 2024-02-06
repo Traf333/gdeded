384d7198911e0a955054e903f356fb40fb4d77e3
Igor Trofimov <igor@litentry.com>
2024-02-06 15:33:52 +0200

    check param

--- src/routes/play/[id]/+page.ts
+++ src/routes/play/[id]/+page.ts
@@ -4,9 +4,11 @@ export const prerender = true;
 
 export const load = async ({ params }) => {
 	const response = await scenarioById(params.id);
-
-	const { speeches: sp, ...rest } = response.data.findScenarioByID;
-	const play = rest;
-	const speeches = sp.data;
-	return { play, speeches };
+	if (response.data.findScenarioByID) {
+		const { speeches: sp, ...rest } = response.data.findScenarioByID;
+		const play = rest;
+		const speeches = sp.data;
+		return { play, speeches };
+	}
+	return {};
 };
