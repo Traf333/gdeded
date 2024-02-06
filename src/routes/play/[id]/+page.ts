3be2ea61b39405573006abbe9a791148481b358f
Igor Trofimov <igor@litentry.com>
2024-02-06 15:28:36 +0200

    check commit

--- src/routes/play/[id]/+page.ts
+++ src/routes/play/[id]/+page.ts
@@ -3,9 +3,10 @@ import { scenarioById } from '../../../api/scenario';
 export const prerender = true;
 
 export const load = async ({ params }) => {
-  const response = await scenarioById(params.id);
-  const { speeches: sp, ...rest } = response.data.findScenarioByID;
-  const play = rest;
-  const speeches = sp.data;
-  return { play, speeches };
+	const response = await scenarioById(params.id);
+
+	const { speeches: sp, ...rest } = response.data.findScenarioByID;
+	const play = rest;
+	const speeches = sp.data;
+	return { play, speeches };
 };
